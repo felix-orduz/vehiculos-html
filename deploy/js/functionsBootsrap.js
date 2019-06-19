b2fc110652d0fd69f011c8d5b824312f464d92b8
harolconde <harolconde@gmail.com>
2019-06-19 06:57:47 -0500

    Update all version mobil and section our fleet

--- deploy/js/functionsBootsrap.js
+++ deploy/js/functionsBootsrap.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
-    
+
+    // Carruseles Autos
+    $('.owl-fleet').owlCarousel({
+        loop:true,
+        margin:30,
+        nav:true,
+        dots:true,
+        pagination: true,
+        responsive:{
+            0:{
+	            items:1
+	        },
+			490:{
+	            items:2
+	        },
+			710:{
+	            items:3
+	        },
+			920:{
+	            items:4
+	        }
+        }
+    })
 })
